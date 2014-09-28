9623cd8d2999761ffa9b5702a93b1de4f44006ae
StevenNdaye <stevenndaye@gmail.com>
2014-09-28 20:38:53 -0300

    Modified some wordings in the router.js

--- public/js/router/router.js
+++ public/js/router/router.js
@@ -1,11 +1,11 @@
 movieStubApp.config(function($routeProvider) {
   $routeProvider
     .when('/', {
-      templareUrl: 'tmpl/home.html',
+      templateUrl: 'tmpl/home.html',
       controller: 'movieStubController'
     })
     .when('/movie/:id', {
-      templareUrl: 'tmpl/movie.html',
+      templateUrl: 'tmpl/movie.html',
       controller: 'movieDetailsController'
     })
     .otherwise({
